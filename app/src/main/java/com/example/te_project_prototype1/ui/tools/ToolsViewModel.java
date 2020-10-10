2067c463102c90b2bd421155bb0c934f0121c99a
dhwaniserai <dhwaniserai2000@gmail.com>
2020-10-10 17:37:42 +0530

    Initial commit

---
+++ app/src/main/java/com/example/te_project_prototype1/ui/tools/ToolsViewModel.java
@@ -0,0 +1,19 @@
+package com.example.te_project_prototype1.ui.tools;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class ToolsViewModel extends ViewModel {
+
+    private MutableLiveData<String> mText;
+
+    public ToolsViewModel() {
+        mText = new MutableLiveData<>();
+        mText.setValue("This is tools fragment");
+    }
+
+    public LiveData<String> getText() {
+        return mText;
+    }
+}
---
+++ app/src/main/java/com/example/te_project_prototype1/userInfoActivity.java
@@ -0,0 +1,14 @@
+package com.example.te_project_prototype1;
+//priya dashboard activity
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class userInfoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_info);
+    }
+}

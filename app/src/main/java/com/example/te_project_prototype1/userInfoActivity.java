6fbd51aeed534538994b63fc4f09f08ab8cea58f
kirtipatil18 <2018.kirti.patil@ves.ac.in>
2020-10-11 14:19:12 +0530

    2nd trial 11-10-2020

--- app/src/main/java/com/example/te_project_prototype1/userInfoActivity.java
+++ app/src/main/java/com/example/te_project_prototype1/userInfoActivity.java
@@ -3,6 +3,8 @@ package com.example.te_project_prototype1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.method.LinkMovementMethod;
+import android.widget.TextView;
 
 public class userInfoActivity extends AppCompatActivity {
 
@@ -10,5 +12,7 @@ public class userInfoActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_info);
+        TextView text = (TextView) findViewById(R.id.link1);
+        text.setMovementMethod(LinkMovementMethod.getInstance());
     }
 }
